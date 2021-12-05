e7d35551d2733b1e3ffe0726604d406f5acd1ce0
chenst <2757045143@qq.com>
2021-12-05 14:30:15 +0800

    swagger 页面添加

--- open_api/__init__.py
+++ open_api/__init__.py
@@ -1,6 +1,8 @@
+import os
 import json
 from pydantic import AnyUrl
 from .models import APISpec, Components, ExternalDocumentation, Info, SecurityScheme
+from flask import Blueprint, render_template
 
 
 class OpenApi:
@@ -10,7 +12,7 @@ class OpenApi:
         self.api_name = 'openapi'
         self.openapi_version = '3.0.3'
         self.info = Info(title='OpenAPI', version='1.0.0')
-        self.api_json_url = f'/{self.api_name}/{self.api_name}.json'
+        self.api_doc_url = f'//{self.api_name}.json'
         self.markdown_url = f'/{self.api_name}/markdown'
         self.components = Components()
         self.components_schemas = {}
@@ -25,7 +27,55 @@ class OpenApi:
         self.paths = dict()
 
         self.securitySchemes = SecurityScheme
+        self.docExpansion = 'list'
+        self.oauth_config = dict()
+        self.register_template()
 
+    def register_template(self):
+        _here = os.path.dirname(__file__)
+        template_folder = os.path.join(_here, 'templates')
+        static_folder = os.path.join(template_folder, 'static')
+
+        blueprint = Blueprint(
+            self.api_name,
+            __name__,
+            url_prefix=f'/{self.api_name}',
+            template_folder=template_folder,
+            static_folder=static_folder
+        )
+        blueprint.add_url_rule(
+            rule=self.api_doc_url,
+            endpoint=self.api_name,
+            view_func=lambda: self.api_doc
+        )
+        blueprint.add_url_rule(
+            rule='/redoc',
+            endpoint='redoc',
+            view_func=lambda: render_template("redoc.html", api_doc_url=f'{self.api_name}.json')
+        )
+        blueprint.add_url_rule(
+            rule='/swagger',
+            endpoint='swagger',
+            view_func=lambda: render_template(
+                "swagger.html",
+                api_doc_url=f'{self.api_name}.json',
+                docExpansion=self.docExpansion,
+                oauth_config=self.oauth_config.dict() if self.oauth_config else None
+            )
+        )
+        blueprint.add_url_rule(
+            rule='/markdown',
+            endpoint='markdown',
+            view_func=lambda: self.export_to_markdown()
+        )
+        blueprint.add_url_rule(
+            rule='/',
+            endpoint='index',
+            view_func=lambda: render_template("index.html")
+        )
+        self.app.register_blueprint(blueprint)
+
+    @property
     def api_doc(self):
         spec = APISpec(
             openapi=self.openapi_version,
